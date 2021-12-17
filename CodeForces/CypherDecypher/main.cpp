345707fe4e7100018b101a95e65c7974fd5fc23d
Abraham Villalba <abrahamvillalba00@gmail.com>
2021-12-17 12:36:05 -0700

    Se agrego el primer borrador de la solucion.

--- CodeForces/CypherDecypher/main.cpp
+++ CodeForces/CypherDecypher/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
 
+const int LIM_SUP = 1000000;
 using namespace std;
+
+void IniciarCriba(bool criba[]);
+bool EsPrimoCriba(int num, bool criba[]);
+unsigned ContarPrimos(int inicio, int fin, const bool criba[]);
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cin.tie(0);
+    std::ios_base::sync_with_stdio(false);
+    bool primos[LIM_SUP + 2] = { true };
+    IniciarCriba(primos);
+    int t,i,j;
+    cin >> t;
+    for(int c=1; c <= t; ++c){
+        cin >> i >> j;
+        cout << ContarPrimos(i,j,primos) << '\n';
+    }
     return 0;
+}
+
+void IniciarCriba(bool criba[])
+{
+    for(int i = 2; i <= LIM_SUP; ++i){
+        if(criba[i]){
+            for(int j = i*2; j <= LIM_SUP; j+=i)
+                criba[j] = false;
+        }
+    }
+}
+
+bool EsPrimoCriba(int num, const bool criba[])
+{
+    return criba[num];
+}
+
+unsigned ContarPrimos(int inicio, int fin, const bool criba[])
+{
+    unsigned numPrim = 0;
+    for(int i = inicio; i <= fin; ++i){
+        if(EsPrimoCriba(i, criba)) ++numPrim;
+    }
+    return numPrim;
 }
